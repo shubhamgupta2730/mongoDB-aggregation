6ce66cccefe129a3210d4640ccf388a3b080bc46
Shubham Gupta <shubhamgupta2730@gmail.com>
2024-06-21 15:58:34 +0530

    updated controllers for search, sort and pagination

--- controllers/listOfPropertyController.js
+++ controllers/listOfPropertyController.js
@@ -2,28 +2,36 @@ const Property = require('../models/property');
 
 const listOfProperties = async (req, res) => {
   try {
-    const propertyTypeName = req.query.property_type_name;
-    if (!propertyTypeName) { 
-      return res.status(400).json({
-        error: 'property_type_name query parameter is required'
-      });
-    }
+    const { pageNo = 1, limit = 5, searchName = '', sort = 'name', property_type_name: propertyTypeName } = req.query;
 
-    const properties = await Property.find({
-      property_type: propertyTypeName 
-    });
+    if (!propertyTypeName) {
+      return res.status(400).json({ error: 'property_type_name query parameter is required' });
+    }
+    
+    const query = {
+      property_type: propertyTypeName,
+      name: new RegExp(searchName, 'i') 
+    };
+
+
+    const properties = await Property.find(query)
+      .select('_id name property_type')
+      .skip((pageNo - 1) * limit)
+      .limit(limit)
+      .sort(sort);
+
+    const totalProperties = await Property.countDocuments(query);
+    const totalPages = Math.ceil(totalProperties / limit);
 
     return res.status(200).json({
-      properties
+      properties,
+      totalProperties,
+      totalPages,
+      currentPage:pageNo
     });
-
   } catch (error) {
-    return res.status(500).json({ 
-      error: error.message
-    });
+    return res.status(500).json({ error: error.message });
   }
-}
+};
 
-module.exports = {
-  listOfProperties
-}
+module.exports ={ listOfProperties};
--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -6,14 +6,14 @@ const Property = require('../models/property');
 
 const propertyTypes = async (req, res) => {
   try {
-    const { page = 1, limit = 5, search = '', sort = 'property_type' } = req.query;
+    const { pageNo = 1, limit = 5, searchName = '', sort = 'property_type' } = req.query;
 
 
     const propertyList = await Property.aggregate([
       {
         //searching the property by name field: 
         $match: {
-          name: { $regex: search, $options: 'i' }
+          name: { $regex: searchName, $options: 'i' }
         }
       },
       {
@@ -38,15 +38,16 @@ const propertyTypes = async (req, res) => {
         }
       },
       {
-        $skip: (page - 1) * limit
+        $skip: (pageNo - 1) * limit
       },
       {
         $limit: parseInt(limit)
       }
     ]);
+    
     res.status(200).json({
       propertyList,
-      currentPage: parseInt(page),
+      currentPage: parseInt(pageNo),
       totalPages: Math.ceil(propertyList.length / limit),
       totalResults: propertyList.length
     });
@@ -55,6 +56,7 @@ const propertyTypes = async (req, res) => {
       error: error.message,
     });
   }
+
 };
 
 module.exports = {
